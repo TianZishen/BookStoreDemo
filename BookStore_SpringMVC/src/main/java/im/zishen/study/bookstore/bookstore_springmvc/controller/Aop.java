4c6861751087e3e7aa1202b17c96695c835e2924
TianZishen <tzs1998@live.com>
2022-06-23 18:49:54 +0800

    AOP maven

---
+++ BookStore_SpringMVC/src/main/java/im/zishen/study/bookstore/bookstore_springmvc/controller/Aop.java
@@ -0,0 +1,10 @@
+package im.zishen.study.bookstore.bookstore_springmvc.controller;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class Aop {
+
+}
