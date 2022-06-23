85a5b9dd993e323edeff14f04bca3f295a0bde86
TianZishen <tzs1998@live.com>
2022-06-23 22:19:26 +0800

    11

--- BookStore_SpringMVC/src/main/java/im/zishen/study/bookstore/bookstore_springmvc/controller/HelloController.java
+++ BookStore_SpringMVC/src/main/java/im/zishen/study/bookstore/bookstore_springmvc/controller/HelloController.java
@@ -1,17 +1,17 @@
 package im.zishen.study.bookstore.bookstore_springmvc.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@org.springframework.stereotype.Controller
+@Controller
 public class HelloController{
     @RequestMapping("/hello")
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         System.out.println("hello");
-        ModelAndView mav = new ModelAndView("index.jsp");
+        ModelAndView mav = new ModelAndView("index");
         mav.addObject("message", "Hello SpringMVC");
         return mav;
     }
